763b18b284a02e4f293c8667aa47b686e48d509a
Andrew Parrish <m.andrewparrish@gmail.com>
2021-01-30 14:22:31 -0500

    Cleaning up generators

---
+++ lib/generators/perigren_github_webhooks/install_generator.rb
@@ -0,0 +1,28 @@
+module PerigrenGithubWebhooks
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
+
+    include Rails::Generators::Migration
+
+    def create_initializer_file
+      copy_file "perigren_github_webhooks.rb", "config/initializers/perigren_github_webhooks.rb"
+    end
+
+    def copy_migrations
+      if self.class.migration_exists?('db/migrate', 'perigren_github_webhooks_create_github_models')
+        say_status('skipped', "Migration 'perigren_github_webhooks_create_github_models' already exists")
+      else
+        migration_template(
+          'perigren_github_webhooks_create_github_models.rb.erb',
+          'db/migrate/perigren_github_webhooks_create_github_models.rb'
+        )
+      end
+    end
+
+    private
+
+    def self.next_migration_number(path)
+      Time.now.utc.strftime('%Y%m%d%H%M%S')
+    end
+  end
+end
